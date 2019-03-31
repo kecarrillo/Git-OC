512d9c1b98ebc6075c0f5923b06c0436f1a2b59e
greyr <kevin.carrillo@imie.fr>
2019-03-31 12:47:25 +0200

    Suppression du Hello World de fichier1.py(fonctionnalité de retranscription de saisie passe ligne 1 et 2)

--- fichier1.py
+++ fichier1.py
@@ -1,4 +1,4 @@
-print("Hello world")
-
 saisie = input("Saisissez une phrase, svp: ")
 print(f'Vous avez saisi: {saisie}')
+=======
+>>>>>>> parent of 669ffb0... Update de fichier1.py via GitHub: affichage du Hello World (ligne1)
